546c0e2296cf2371cea3226c1eb41015fc233e60
Leandro <lealabra87@gmail.com>
2019-04-02 14:03:04 -0300

    Finish implementation of blog functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
   end
 
   def create
-  
+
     @post = Post.new(post_params)
 
     if @post.save
@@ -27,10 +27,31 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
   private
 
   def post_params
    params.require(:post).permit(:title, :content)
   end
-  
+
 end
